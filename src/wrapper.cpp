3a4f3837fa99383ab9e1373ca50e744cde963908
Fabrice Jacquemet <fabrice.jacquemet@gmail.com>
2015-01-19 01:41:32 +0100

    add fields

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -21,10 +21,26 @@ void Wrapper::wrap(const std::string& in_str, std::string& out_str)
 
 void Wrapper::wrap(const Shareasale_record& in_record, Prestashop_record& out_record)
 {
+  int i=0;
+  out_record.setField(i++, in_record.at(1));
+  out_record.setField(i++, "1");
+  out_record.setField(i++, in_record.at(2));
+  out_record.setField(i++, "44");  // TODO cat #
+  out_record.setField(i++, in_record.at(9));
+  out_record.setField(i++, "0");
+  out_record.setField(i++, "0");
+  out_record.setField(i++, "1");
+  out_record.setField(i++, "");
+  double old_price = atof(in_record.at(9).c_str());
+  if (old_price == 0.0)
+    out_record.setField(i++, "0");
+  else {
+    double new_price = atof(in_record.at(8).c_str());
+    double reduction = (old_price - new_price) / old_price;
+    out_record.setField(i++, std::to_string(static_cast<int>(reduction)));
+  }
   // TODO
-  // ex : map 1 -> 0 and 0 -> 1
-  out_record.setField(1, in_record.at(0));
-  out_record.setField(0, in_record.at(1));
+  //"2000-01-01","2099-12-31",$1,$21,$3,"","","","","0","0","0","0","99","1","","","","",$2,$12,"","","","","","in stock","","1","2000-01-01","2000-01-01","1",$6,"1","","","","0","0","0","0","0","0","0","0"
 }
 
 Wrapper::~Wrapper()
